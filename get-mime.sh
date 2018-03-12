7a7b455a4789a8ef92f8809698f53ed7cacde350
David Gwartney <david.gwartney@import.io>
2018-03-12 16:50:31 -0700

    checkpoint

--- get-mime.sh
+++ get-mime.sh
@@ -3,7 +3,7 @@
 for sample_file in $(find sample-files -type f  -print)
 do
     mime_type=$(file --mime-type -b ${sample_file})
-    echo "$(basename ${sample_file}) is mime-type ${mime_type}"
+    echo "$(basename ${sample_file}) is mime-type: ${mime_type}"
 done
 
 
